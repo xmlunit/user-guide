51be6b6091c0095a48cc0573aca823f9e11f475f
Stefan Bodewig <stefan.bodewig@freenet.de>
2016-01-02 18:07:04 +0100

    add infrastructure for testing user guide examples

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ This repository mostly exists for its
 [Wiki](https://github.com/xmlunit/user-guide/wiki) where the new User's
 Guide will be crafted.
 
+The `examples` subdirectory contains example code from the user guide
+and ensures it actually does what the userguide claims it to do.
 
---
+++ examples/java/src/main/java/org/xmlunit/userguide_examples/diff/IgnoreAttributeDifferenceEvaluator.java
@@ -0,0 +1,44 @@
+/*
+  This file is licensed to You under the Apache License, Version 2.0
+  (the "License"); you may not use this file except in compliance with
+  the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package org.xmlunit.userguide_examples.diff;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.Attr;
+import org.xmlunit.diff.Comparison;
+import org.xmlunit.diff.ComparisonResult;
+import org.xmlunit.diff.DifferenceEvaluator;
+
+public final class IgnoreAttributeDifferenceEvaluator implements DifferenceEvaluator {
+
+    private String attributeName;
+
+    public IgnoreAttributeDifferenceEvaluator(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    @Override
+    public ComparisonResult evaluate(Comparison comparison, ComparisonResult outcome) {
+        if (outcome == ComparisonResult.EQUAL) return outcome; // only evaluate differences.
+        final Node controlNode = comparison.getControlDetails().getTarget();
+        if (controlNode instanceof Attr) {
+            Attr attr = (Attr) controlNode;
+            if (attr.getName().equals(attributeName)) {
+                return ComparisonResult.SIMILAR; // will evaluate this difference as similar
+            }
+        }
+        return outcome;
+    }
+} 
+
